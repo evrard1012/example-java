d7108180b1e165cb84f6f2e708bebdc77b06dd06
evrard1012 <evrardcabrel@gmail.com>
2019-11-10 14:32:17 +0000

    second

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,14 @@
 
 public class HelloWorld {
 
+	public String getMessage(String name) {
+		if (name == null || name == "")
+			return "Hello World";
+		else
+			return "Hello " + name;
+	}
+
+	public long sum(int a, int b) {
+		return a + b;
+	}
 }
